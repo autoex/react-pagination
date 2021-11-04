8f5de7cc9ca86f415c0319b05332c4a9ca8c99f1
autoex <autoex@gmail.com>
2021-11-04 12:24:08 +0200

    basic pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css'
+import axios from "axios";
+import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Pagination from "@mui/material/Pagination";
+import Link from "@mui/material/Link";
+
+const BASE_URL = `http://hn.algolia.com/api/v1/search?query=`;
 
 const App = () => {
-    return (
-        <div>
+    const [posts, setPosts] = useState([]);
+    const [query, setQuery] = useState('react');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageQty, srtPageQty] = useState(0);
 
-        </div>
+    useEffect(() => {
+        axios(BASE_URL + query + `&page=${currentPage}`)
+            .then(({data}) => {
+                console.log(data)
+                setCurrentPage(data.page);
+                srtPageQty(data.nbPages);
+                setPosts(data.hits)
+            })
+    }, [query, currentPage]);
+    return (
+        <Container>
+            <TextField fullWidth id="filled-basic" label="Search" variant="filled" value={query}
+                       onChange={e => setQuery(e.target.value)}/>
+            <Stack spacing={2}>
+                {!!posts && <ul>
+                    {posts.map(post => <li key={post.objectID}><Link href={post.url}>{post.title}</Link></li>)}
+                </ul>}
+                {!!pageQty && (
+                    <Pagination
+                        count={pageQty}
+                        page={currentPage}
+                        onChange={(_, num) => setCurrentPage(num)}/>
+                )}
+            </Stack>
+        </Container>
     );
 };
 
